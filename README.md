19dae50ae95b327549392fca1464d57459f17bd9
elmehdirida <elmehdi.fst@uhp.ac.ma>
2023-12-04 15:32:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CryptoProject
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.3.
+
+# project description
+This project is simple project for encrypting and decrypting text using Vigenere cipher and save it in database.
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
+import {MatTableModule} from "@angular/material/table";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatTableModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatSnackBarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
